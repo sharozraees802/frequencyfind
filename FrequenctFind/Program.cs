8456faa203386dac782225c0e5a53ef4adda9f48
sharozraees <52934734+sharozraees802@users.noreply.github.com>
2020-10-23 12:09:43 +0500

    frequecy find alphabat work is done

--- FrequenctFind/Program.cs
+++ FrequenctFind/Program.cs
@@ -10,6 +10,34 @@ namespace FrequenctFind
     {
         static void Main(string[] args)
         {
+            //Console.WriteLine("counting word frequency in a paragraph\n");
+
+            //string paragraph = "W W BR Y BL BL W W Y G G W W BL BR BL BR BL BL BR Y Y BL G W BL W W BL W BL BR BL Y BR G BR G W BR Y G BR G W BR Y W BL W BR G Y G Y";
+
+            //string[] words = paragraph.ToLower().Split(' ');
+
+            //Dictionary<string, int> myDictionry = new Dictionary<string, int>();
+
+            //foreach (string word in words)
+            //{
+            //    if (!myDictionry.ContainsKey(word))
+            //    {
+            //        myDictionry.Add(word, 1);
+            //    }
+            //    else
+            //    {
+            //        myDictionry[word]++;
+            //    }
+            //}
+
+            //foreach (string key in myDictionry.Keys)
+            //{
+            //    Console.WriteLine(key + " : " + myDictionry[key] + "\n");
+
+            //}
+
+
+            Console.ReadLine();
         }
     }
 }
